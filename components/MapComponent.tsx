25acf73e88d5ef5719f29c0c750da0aab9720c96
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-10-29 06:39:42 +0530

    adding & viewing marker

--- components/MapComponent.tsx
+++ components/MapComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 import { ViewportProps } from "react-map-gl";
 
 interface MapComponentProps {
@@ -13,24 +13,37 @@ This component is responsible for rendering the map. This component has to show 
 markers bookmarked by users.
 */
 const MapComponent = (props: MapComponentProps): JSX.Element => {
+  const [markers, setMarkers] = useState<any[]>([]);
+  const handleClick = ({ lngLat: [longitude, latitude] }) =>
+    setMarkers((markers) => [...markers, { longitude, latitude }]);
   const [viewport, setViewport] = useState<ViewportProps>({
-    width: 400,
-    height: 400,
     latitude: 37.7577,
     longitude: -122.4376,
-    zoom: 8,
+    zoom: 14,
   });
 
   return (
     <div className={props.className}>
       <h3>Map</h3>
       <ReactMapGL
+        onClick={handleClick}
+        width="50vw"
+        height="50vh"
         {...viewport}
         mapboxApiAccessToken={MAPBOX_TOKEN}
         onViewportChange={(nextViewport: ViewportProps) =>
           setViewport(nextViewport)
         }
-      />
+      >
+        {markers.length
+          ? markers.map((m, i) => (
+              // <Marker /> just places its children at the right lat lng.
+              <Marker {...m} key={i}>
+                {`📌${m.longitude}, ${m.latitude}`}
+              </Marker>
+            ))
+          : null}
+      </ReactMapGL>
     </div>
   );
 };
